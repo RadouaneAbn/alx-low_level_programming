7ff57138e13d6e1e3f4e1c0e5e32979fdc22ddec
radouane abounouas <radouaneabn@gmail.com>
2023-08-10 14:53:50 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(){
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return 0;
+/**
+ * main - print the phrase inside "" followed by new line
+ * Return: always return (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
