6f0a18b19685fc88bced073751827b574a84cd5d
radouane abounouas <radouaneabn@gmail.com>
2023-08-10 14:49:38 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(){
-       puts("\"Programming is like building a multilingual puzzle");
-       return 0;
+/**
+ * main - prints whats inside the " "
+ * Return: always return (0)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
