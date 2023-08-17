a1ae17c44945add9bb3f1f8a36198eb302c14be5
radouane.abounouas <radouaneabn@gmail.com>
2023-08-17 14:47:53 +0100

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - printd the largest prime factor of the number 612852475143
+ * Retune: always return 0
+ */
+
+int main(void)
+{
+	unsigned long int n = 612852475143;
+	int i = 2;
+
+	while (n != 1)
+	{
+		if (n % i == 0)
+			n /= i;
+		else
+			i += 1;
+	}
+	printf("%d\n", i);
+	return (0);
+}
