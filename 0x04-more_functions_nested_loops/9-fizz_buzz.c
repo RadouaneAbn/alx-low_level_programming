7b3d5688051084ceb503e56cd498bd409eee8791
radouane.abounouas <radouaneabn@gmail.com>
2023-08-17 13:27:52 +0100

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 to 100, replacing multiplies of 3 with Fizz and 5 with Buzz and both with FizzBuzz
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if  (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 0 || i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
