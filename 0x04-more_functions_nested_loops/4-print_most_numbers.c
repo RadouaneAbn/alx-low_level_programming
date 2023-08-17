afd3d04e32c3696705e7081e62814c4f61cf5388
radouane.abounouas <radouaneabn@gmail.com>
2023-08-17 11:52:42 +0100

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that print the numbers from 0 to 9
+ * Return: always return void
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	i++;
+	}
+	_putchar('\n');
+}
