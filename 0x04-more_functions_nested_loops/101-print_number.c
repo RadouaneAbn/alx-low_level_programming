4cdad3804a110a8aec660cf299ae7910dd8905ce
radouane.abounouas <radouaneabn@gmail.com>
2023-08-17 17:11:26 +0100

    Task 6

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,14 +10,16 @@
 
 void print_number(int n)
 {
+	unsigned int m = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		m = -n;
 	}
 
-	if (n / 10 > 0)
-		print_number(n / 10);
+	if (m / 10 > 0)
+		print_number(m / 10);
 
-	_putchar(n % 10 + '0');
+	_putchar(m % 10 + '0');
 }
