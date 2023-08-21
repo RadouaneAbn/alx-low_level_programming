9310465f2f104a1f1241db0d8f47bde4ab9098e4
radouane.abounouas <radouaneabn@gmail.com>
2023-08-21 17:54:58 +0100

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,12 +8,14 @@
 
 int _atoi(char *s)
 {
-	int i = 0, T = 0, end = 0, sign = 1;
+	int i = 0, end = 0, sign = 1;
+	unsigned int T = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
+
 		while (s[i] >= '0' && s[i] <= '9')
 		{
 			end = 1;
