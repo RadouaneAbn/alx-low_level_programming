b7e034ca594b50e8c687442af429d822f557a03d
radouane.abounouas <radouaneabn@gmail.com>
2023-08-21 12:05:33 +0100

    task 0, 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this function will reset the value of n to 98
+ * @n: the pointer variable
+ * Return: always return void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - this function will swap the value of two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: always return void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
