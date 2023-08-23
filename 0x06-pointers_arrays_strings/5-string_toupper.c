d3fe2c0705d4c8924533657df4290dd317311534
radouane.abounouas <radouaneabn@gmail.com>
2023-08-23 18:06:37 +0100

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - this function change all lowercase letters to uppercase
+ * @c: the string
+ * Return: return string with all letters changed to uppercase
+ */
+
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i])
+	{
+		if (c[i] > 'a' && c[i] < 'z')
+			c[i] -= 32;
+	i++;
+	}
+	return (c);
+}
