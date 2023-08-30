3623005a183921f7bae89f8a2206d796770aa727
radouane.abounouas <radouaneabn@gmail.com>
2023-08-30 21:09:18 +0100

    Task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - gets the lenght of string
+ * @s: the string
+ * Return: return the lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * compare - this function check if string is palindrome
+ * @s: the string
+ * @i: index of first char in s
+ * @j: index of last char in s
+ * Return: return 1 if string is palindrome, 0 if not
+ */
+
+int compare(char *s, int i, int j)
+{
+	if (s[i] != s[j])
+		return (0);
+	if (s[i] == s[j] && i <= j)
+		return (compare(s, ++i, --j));
+	return (1);
+}
+
+/**
+ * is_palindrome - this function gets the string ready to be checked
+ * @s: the string
+ * Return: 1 if palidrome, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int n;
+
+	n = _strlen(s) - 1;
+	return (compare(s, 0, n));
+}
