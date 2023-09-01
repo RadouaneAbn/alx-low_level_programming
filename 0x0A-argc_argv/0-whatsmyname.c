9f1618571bf378c10332cae326125214044ba2a0
radouane.abounouas <radouaneabn@gmail.com>
2023-09-01 06:27:21 +0100

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * Description: this program will print its name
+ * @argc: the size of argv array
+ * @argv: array of arguments
+ * Return: always return 0
+ */
+
 int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
