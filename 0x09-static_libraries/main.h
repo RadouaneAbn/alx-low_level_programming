117cbc87831a047177cedc6402c7f311a5da6c2a
radouane.abounouas <radouaneabn@gmail.com>
2023-09-01 16:57:57 +0100

    Task 0

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _isupper(int c);
+char *_strcat(char *dest, char *src);
+int _isdigit(int c);
+char *_strncat(char *dest, char *src, int n);
+int _atoi(char *s);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+int _islower(int c);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+int _isalpha(int c);
+int _abs(int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
