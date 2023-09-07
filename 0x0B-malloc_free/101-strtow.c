178753404addcf0b99f8debe65dd5abd9e867760
radouane.abounouas <radouaneabn@gmail.com>
2023-09-07 01:43:46 +0100

    Task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * word_count - this function find the number of words in a string
+ * @s: the string
+ * Return: number of words
+ */
+
+int word_count(char *s)
+{
+	int i, word = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ' || s[i] == '\0')
+			word++;
+	}
+
+	return (word);
+}
+
+/**
+ * _strlen - this function compute the lenght of a string
+ * @s: pointer to the string
+ * Return: return the lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * **strtow - this function splits a string into words
+ * @str: the string
+ * Return: pointer to an array of words if success or NULL if failure
+ */
+
+char **strtow(char *str)
+{
+	char **A, *tmp;
+	int i, k = 0, len, words, c = 0, start, end;
+
+	len = _strlen(str);
+	words = word_count(str);
+	if (words == 0)
+		return (NULL);
+
+	A = (char **) malloc(sizeof(char *) * (words + 1));
+	if (A == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				A[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	A[k] = NULL;
+
+	return (A);
+}
