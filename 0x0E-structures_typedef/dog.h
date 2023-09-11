164b5c4b678a662e7d71dc7625fef49d788f6f35
radouane.abounouas <radouaneabn@gmail.com>
2023-09-11 18:29:00 +0100

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/**
+ * struct dog - this is a structure that contains information about a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: this structure contains information about a dog
+ *	its name, age and its owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
