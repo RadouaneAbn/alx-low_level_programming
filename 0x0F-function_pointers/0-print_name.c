06937349ed9ee5884cc576b31b144ebe81b375d2
radouane.abounouas <radouaneabn@gmail.com>
2023-09-13 18:09:36 +0100

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name in two different ways
+ * @name: the name
+ * @f: the way name should be printed
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
