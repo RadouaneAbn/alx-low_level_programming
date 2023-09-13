28c720f32f8d0e675d43f020fe3ba6f56b1d4d22
radouane.abounouas <radouaneabn@gmail.com>
2023-09-13 19:04:45 +0100

    Check 7 : try 4

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 
-	if (size == 0 || array == NULL)
+	if (size == 0 || array == NULL || action == NULL)
 		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
