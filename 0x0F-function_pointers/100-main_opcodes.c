6e678b925dcc3ac225b08897bf0d112e77976141
radouane.abounouas <radouaneabn@gmail.com>
2023-09-14 01:15:39 +0100

    Task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Description: this program prints n bytes its main function
+ *
+ * Return: 0 if success
+ *	exit with the status 1 if the number of arguments is not 2
+ *	or with 2 if the number of bytes is negative
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+	char *A;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	A = (char *)&main;
+	for (i = 0; i < bytes; i++)
+		printf("%.2hhx ", A[i]);
+	printf("\n");
+	return (0);
+}
