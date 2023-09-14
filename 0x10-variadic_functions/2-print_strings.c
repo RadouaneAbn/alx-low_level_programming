9822753f689bdabab80ec096c3ebee4df7ddfc39
radouane.abounouas <radouaneabn@gmail.com>
2023-09-14 15:50:39 +0100

    Task 2 : Fix NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,12 +24,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(args, char *);
-		if (*s != '\0')
+
+		if (s != NULL)
 			printf("%s", s);
 		else
 			printf("(nil)");
-		if (i < n - 1 && *separator)
-			printf("%s", separator);
+		if (i < n - 1)
+			if (separator)
+				printf("%s", separator);
 	}
 	va_end(args);
 	printf("\n");
