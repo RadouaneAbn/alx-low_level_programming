276d8a2a341a769e080b361368a793dfc0febdbd
radouane.abounouas <radouaneabn@gmail.com>
2023-09-14 15:22:43 +0100

    Task 1 : check 6

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,9 +23,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, int));
-		if (*separator && i < n - 1)
-			printf("%s", separator);
+		if (i == 0 || separator == NULL)
+			printf("%d", va_arg(args, int));
+		else if (i > 0)
+			printf("%s%d", separator, va_arg(args, int));
 	}
 	va_end(args);
 	printf("\n");
