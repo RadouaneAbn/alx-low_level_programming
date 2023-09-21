1cb9bfdb33523d934032c2c55578297cc7e8d47d
radouane.abounouas <radouaneabn@gmail.com>
2023-09-21 16:36:12 +0100

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,7 +14,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *current;
 	list_t *new = malloc(sizeof(list_t));
 
-	if (new == NULL)
+	if (new == NULL || str == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
