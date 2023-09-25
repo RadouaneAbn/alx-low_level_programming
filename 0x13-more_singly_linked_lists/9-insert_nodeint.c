611351cdc6bc5e2f6dbb152b2365f4860a72cc1a
radouane.abounouas <radouaneabn@gmail.com>
2023-09-25 17:56:39 +0100

    Task 9 : Fix 6,9,10,11

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,14 +16,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *tmp;
 
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
 	if (idx == 0)
 	{
+		new->n = n;
 		new->next = *head;
 		*head = new;
 		return (new);
 	}
-	while (--idx != 0)
+	while (--idx)
 	{
 		if (current == NULL)
 		{
@@ -34,8 +37,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 
-	if (new == NULL)
-		return (NULL);
 
 	tmp = current->next;
 	current->next = new;
