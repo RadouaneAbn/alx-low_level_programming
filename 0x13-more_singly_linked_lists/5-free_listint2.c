ffc74ac249667c45c6bc2ecee041ab56f687c745
radouane.abounouas <radouaneabn@gmail.com>
2023-09-27 09:07:27 +0100

    Task 5 : Fix return

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,15 +7,15 @@
 
 void free_listint2(listint_t **head)
 {
-        listint_t *tmp, *current = *head;
+        listint_t *tmp;
 
 	if (head == NULL || *head == NULL)
 		return;
 
-        while (current != NULL)
+        while (*head != NULL)
         {
-                tmp = current;
-                current = current->next;
+                tmp = *head;
+                *head = (*head)->next;
 		free(tmp);
         }
 
