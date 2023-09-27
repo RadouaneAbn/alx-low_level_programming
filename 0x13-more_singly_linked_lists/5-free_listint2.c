6cb697f66184d475307a48978fea6782f493b83d
radouane.abounouas <radouaneabn@gmail.com>
2023-09-27 09:08:45 +0100

    Task 5 : Fix Betty

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,17 @@
 
 void free_listint2(listint_t **head)
 {
-        listint_t *tmp;
+	listint_t *tmp;
 
 	if (head == NULL || *head == NULL)
 		return;
 
-        while (*head != NULL)
-        {
-                tmp = *head;
-                *head = (*head)->next;
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
 		free(tmp);
-        }
+	}
 
 	*head = NULL;
 }
