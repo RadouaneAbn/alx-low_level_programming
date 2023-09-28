2b50ad91effaafb6e0bacb28d65aa49dad94a8c3
radouane.abounouas <radouaneabn@gmail.com>
2023-09-28 12:57:52 +0100

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_binary - this function prints the binary representation of a numbe
+ * @n: the number
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int tmp;
+	int i = 0;
+
+	tmp = n, i = 0;
+
+	if (n == 0)
+		_putchar('0');
+
+	while (tmp >= 1)
+	{
+		tmp >>= 1;
+		i++;
+	}
+	tmp = n;
+
+	i--;
+	while (i >= 0)
+	{
+		tmp = n;
+		tmp >>= i;
+		if (tmp == 1)
+			_putchar(1 + '0');
+		else
+			_putchar(0 + '0');
+		tmp <<= i;
+		n -= tmp;
+		i--;
+	}
+}
