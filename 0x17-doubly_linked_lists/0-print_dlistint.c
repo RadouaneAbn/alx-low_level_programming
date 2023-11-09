c96241d9c0524ec068d91af95d8fda951629f9cb
radouane.abounouas <radouaneabn@gmail.com>
2023-11-09 18:34:21 +0100

    Task 1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - this function prints a doubly linked list
+ * @h: pointer to the head of the linked list
+ *
+ * Return: number of nodes printed
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t n_nodes = 0;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		n_nodes++;
+	}
+	return (n_nodes);
+}
