8f2060d681982fa19c5ae6a2c3331f377e4d258b
radouane.abounouas <radouaneabn@gmail.com>
2023-11-09 19:42:49 +0100

    Task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,18 +18,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new->n = n;
-	if (head == NULL)
-	{
-		new->next = NULL;
-		new->prev = NULL;
-		*head = new;
-	}
-	else
-	{
-		new->prev = NULL;
-		new->next = *head;
-		*head = new;
-	}
+
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
 
 	return (new);
 }
