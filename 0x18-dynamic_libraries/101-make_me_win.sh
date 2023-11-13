931a50f45aee806bf64c70fdddc580485a6b21a5
radouane.abounouas <radouaneabn@gmail.com>
2023-11-13 23:11:50 +0100

    Task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/RadouaneAbn/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -P .. https://github.com/RadouaneAbn/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
