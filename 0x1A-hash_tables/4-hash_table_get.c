f76808d47cf9a11c922456cf757f1b33e7714e9d
radouane abounouas <radouaneabn@gmail.com>
2023-12-01 19:02:17 +0100

    Task 4: NULL if (!ht || !key)

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,6 +14,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
+	if (!ht || !key)
+		return (NULL);
+
 	index = key_index((unsigned char *)key, ht->size);
 	node = ht->array[index];
 
