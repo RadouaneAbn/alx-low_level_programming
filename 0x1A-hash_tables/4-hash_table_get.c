39c544ba602a4804499344528978fa4786a3d662
radouane abounouas <radouaneabn@gmail.com>
2023-12-01 19:12:12 +0100

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,10 +20,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((unsigned char *)key, ht->size);
 	node = ht->array[index];
 
-	if (node)
+	while (node)
 	{
 		if (strcmp(node->key, key) == 0)
 			return (node->value);
+		node = node->next;
 	}
 
 	return (NULL);
