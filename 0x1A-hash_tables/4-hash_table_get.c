6e5b32f68a90fd0755a404b631453a18237a0a3d
radouane abounouas <radouaneabn@gmail.com>
2023-12-01 18:52:52 +0100

    Task 4: handle collision

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,11 +12,16 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *node;
 
 	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
 
-	if (ht->array[index] == NULL)
-		return (NULL);
+	if (node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+	}
 
-	return (ht->array[index]->value);
+	return (NULL);
 }
