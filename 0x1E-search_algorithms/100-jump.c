d3ee6127e823a029ae50ca951b9f2af563091508
RadouaneAbn <radouaneabn.com>
2024-03-13 21:06:45 +0000

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -25,7 +25,10 @@ int jump_search(int *array, size_t size, int value)
 	while (left < size)
 	{
 		printf("Value checked array[%ld] = [%d]\n", left, array[left]);
-		if (value <= array[right])
+		if (value == array[right])
+			return (right);
+
+		if (value < array[right])
 			break;
 
 		left += jump;
